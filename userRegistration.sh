e6f6b75d6de890d5491e64ad5f2a3ae59a95f454
nidhu23 <nidhunanik@gmail.com>
2020-05-29 15:37:37 +0530

    Pattern added for email

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash 
 namePattern="^[A-Z][a-z]{2,}"
-
+emailPattern="^[A-Za-z0-9]*[\-\+\.0-9]*[a-zA-z0-9]+[@][a-z0-9]+[.]+[a-z]{2,3}$"
 check(){
 	if [[ $1 =~ $2 ]]
 	then
@@ -10,7 +10,9 @@ check(){
 	fi
  }
 
-read -p "Enter your first Name " name
-check $name $namePattern
-read -p "Enter your first Name " lastName
-check $lastName $namePattern
+#read -p "Enter your first Name " name
+#check $name $namePattern
+#read -p "Enter your first Name " lastName
+#check $lastName $namePattern
+read -p "Enter your mail id " mail
+check $mail $emailPattern
